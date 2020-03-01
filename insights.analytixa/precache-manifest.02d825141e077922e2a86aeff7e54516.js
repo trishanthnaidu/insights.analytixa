1ea9db121915028b4ffec95005670a29c78a4f8f
Trishant Naidu <trishant.naidu.vc@startv.com>
2020-03-01 23:54:35 +0530

    Updates

---
+++ insights.analytixa/precache-manifest.02d825141e077922e2a86aeff7e54516.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7a4c5d8edb9da9b83031ad7e02112c0",
+    "url": "/insights.analytixa/index.html"
+  },
+  {
+    "revision": "a6c7865d3d8a3ea49b38",
+    "url": "/insights.analytixa/static/css/main.eb5df336.chunk.css"
+  },
+  {
+    "revision": "839a7ef50ce195a62db7",
+    "url": "/insights.analytixa/static/js/2.31591619.chunk.js"
+  },
+  {
+    "revision": "6f5f1c4a5f7f6e382b8aeca1b4ac8d96",
+    "url": "/insights.analytixa/static/js/2.31591619.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6c7865d3d8a3ea49b38",
+    "url": "/insights.analytixa/static/js/main.7484161c.chunk.js"
+  },
+  {
+    "revision": "738d3dae1e1b4614f28b",
+    "url": "/insights.analytixa/static/js/runtime-main.d2ec9a85.js"
+  },
+  {
+    "revision": "8b6a1d7f7b93ac3eb4292cbb58fca1b4",
+    "url": "/insights.analytixa/static/media/logo.8b6a1d7f.svg"
+  },
+  {
+    "revision": "59d8bb2b64fb0c2d52150ef6cee6cb34",
+    "url": "/insights.analytixa/static/media/logo1.59d8bb2b.svg"
+  },
+  {
+    "revision": "485713aa53e30818c07319f9c3e2d5b1",
+    "url": "/insights.analytixa/static/media/somethingWentWrongDark.485713aa.svg"
+  },
+  {
+    "revision": "29bd231bd0b5f65494dc852c74e76768",
+    "url": "/insights.analytixa/static/media/somethingWentWrongLight.29bd231b.svg"
+  }
+]);
